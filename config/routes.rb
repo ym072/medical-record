239cbea1145870135f7832c30b051e83f4b1a377
北村優美 <kitamurayumi@kitamurayuminoMacBook-Air.local>
2025-10-20 15:23:17 +0900

    add:新規ユーザー登録画面作成 #11

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "signup", to: "users#new"
   root 'pages#home'
   get "pages/home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
