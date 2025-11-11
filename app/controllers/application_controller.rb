0f6190afd19cca49810969a8d37c53d5a5a05696
北村優美 <kitamurayumi@kitamurayuminoMacBook-Air.local>
2025-11-11 14:02:23 +0900

    fix:新規子ども登録画面エラー

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_current_kid
 
   protected
 
@@ -20,5 +21,23 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
+  end
+
+  private
+
+  def set_current_kid
+    if params[:kid_id].present?
+      @current_kid = current_user.kids.find_by(id: params[:kid_id])
+      session[:current_kid_id] = @current_kid.id if @current_kid
+    elsif session[:current_kid_id].present?
+      @current_kid = current_user.kids.find_by(id: session[:current_kid_id])
+    else
+      @current_kid = nil
+    end
+  end
+  
+  helper_method :current_kid
+  def current_kid
+    @current_kid
   end
 end
